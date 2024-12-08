59ba3035bf85e2627fc18749c74f527c2b1b1ba0
vinayak-77 <vinayak270603@gmail.com>
2024-12-08 21:12:44 +0530

    Update

--- cuisine-prediction/src/App.js
+++ cuisine-prediction/src/App.js
@@ -24,7 +24,7 @@ function App() {
   const handleSubmit = event => {
     
     event.preventDefault();
-    axios.post('http://127.0.0.1:5000/predict',ingredientList).then(
+    axios.post('https://vinayakg311.pythonanywhere.com/predict',ingredientList).then(
       res => {
         console.log(res);
         setPrediction(res.data.output)});
